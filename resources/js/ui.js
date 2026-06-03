f1785bdfcde1c5ce7253711c10d5be4a1fba406e
Amira BA <amira.benameur3@gmail.com>
2026-06-03 21:56:33 +0200

    Set up modular JavaScript architecture

---
+++ resources/js/ui.js
@@ -0,0 +1,14 @@
+// =========================
+// INITIALIZE ICONS
+// =========================
+
+lucide.createIcons();
+
+// =========================
+// DOM ELEMENTS
+// =========================
+
+// Navigation Elements
+const menuButton = document.querySelector(".menu-button");
+const navMenu = document.querySelector(".main-navigation");
+const navLinks = document.querySelectorAll(".main-navigation a");
